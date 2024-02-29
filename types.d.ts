d3fa78b08ae33f06b62f831a90e509c7b669aaba
Matteus Männiste <matteusmanniste@gmail.com>
2024-02-29 16:05:38 +0200

    chore: modify types

--- types.d.ts
+++ types.d.ts
@@ -1,8 +1,11 @@
 export interface Message {
+  id: string;
   author: string;
   message: string;
+  timestamp: Date;
 }
 
 export interface ShoutboxMessageProps extends Message {
+  formattedTimestamp: string;
   onTagActionClick: (author) => void;
 }
