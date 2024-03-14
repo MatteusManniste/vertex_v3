de4260cc6604bc421226dfd3f81d75116d567d88
Matteus Männiste <matteus.manniste@gmail.com>
2024-03-14 19:21:35 +0200

    feat: create members online component (unfinished)

--- components/MembersOnline.tsx
+++ components/MembersOnline.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import "@/styles/members_online.css";
 
 export const MembersOnline = () => {
@@ -5,15 +7,43 @@ export const MembersOnline = () => {
     <section className="members-online">
       <h1 className="members-online-title">Members Online</h1>
       <ul className="members-online-list">
-        <li>HeartyWarrior</li>
-        <li>DownhillFool</li>
-        <li>SeraphClue</li>
-        <li>LotusHunt</li>
-        <li>RavenMiracle</li>
-        <li>GlowingDonor</li>
-        <li>AboriginalEye</li>
+        <li className="member-online">
+          <Link className="member-online-user" href="/">
+            HeartyWarrior
+          </Link>
+        </li>
+        <li className="member-online">
+          <Link className="member-online-user" href="/">
+            DownhillFool
+          </Link>
+        </li>
+        <li className="member-online">
+          <Link className="member-online-user" href="/">
+            SeraphClue
+          </Link>
+        </li>
+        <li className="member-online">
+          <Link className="member-online-user" href="/">
+            LotusHunt
+          </Link>
+        </li>
+        <li className="member-online">
+          <Link className="member-online-user" href="/">
+            RavenMiracle
+          </Link>
+        </li>
+        <li className="member-online">
+          <Link className="member-online-user" href="/">
+            GlowingDonor
+          </Link>
+        </li>
+        <li className="member-online">
+          <Link className="member-online-user" href="/">
+            AboriginalEye
+          </Link>
+        </li>
       </ul>
-      <span className="members-online-count">Total: 7 members</span>
+      <span className="members-online-count">Total: 9 (Members: 7, Guests: 2)</span>
     </section>
   );
 };
