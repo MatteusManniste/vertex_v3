129a68ccad2fe63028ada278d7b5087049bac637
Matteus Männiste <matteus.manniste@gmail.com>
2024-03-14 19:18:56 +0200

    feat: add service activity check

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,17 @@ import { Shoutbox } from "@/components/Shoutbox";
 import { RecentThreads } from "@/components/RecentThreads";
 import { Topics } from "@/components/Topics";
 import { MembersOnline } from "@/components/MembersOnline";
+import { getServiceStatus } from "@/library/data";
 
-const Home = () => {
+const Home = async () => {
+  const shoutboxStatus = await getServiceStatus("shoutbox");
+
   return (
     <main>
       <Banner />
       <div className="wrapper">
         <div className="container">
-          <Shoutbox />
+          <Shoutbox disabled={!shoutboxStatus} />
           <Topics />
         </div>
         <div className="container2">
